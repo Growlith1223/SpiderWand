41b1a2fde7463ac62de93461a3f9c619ba206623
Growlith1223 <Growlith1223@voltisoft.com>
2016-12-08 00:57:38 -0800

    Add null check to prevent crash

--- src/main/java/spiderwand/events/EventsHandler.java
+++ src/main/java/spiderwand/events/EventsHandler.java
@@ -1,6 +1,7 @@
 package spiderwand.events;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import spiderwand.items.ItemVortexedBlock;
@@ -12,9 +13,11 @@ public class EventsHandler {
 
     @SubscribeEvent
     public void onItemTooltip(ItemTooltipEvent e){
-        if (e.getItemStack().getItem() instanceof ItemVortexedBlock){
-            if (e.getItemStack().getTagCompound().hasKey("block")){
-                e.getToolTip().add("currently holding: \u00A7e" + Block.getBlockById(e.getItemStack().getTagCompound().getInteger("block")).getLocalizedName());
+        if (e.getItemStack().getItem() instanceof ItemVortexedBlock) {
+            if (e.getItemStack().getTagCompound() != null) {
+                if (e.getItemStack().getTagCompound().hasKey("block")) {
+                    e.getToolTip().add("currently holding: \u00A7e" + new ItemStack(Block.getBlockById(e.getItemStack().getTagCompound().getInteger("block")), 1).getDisplayName());
+                }
             }
         }
     }
